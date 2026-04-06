8e1bc67f5a0d3b4a19e399eb63365a4dc0d246a4
t44203676-glitch <t44203676@gmail.com>
2026-04-06 18:16:39 +0300

    Delete htdocs directory

--- htdocs/check_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/database.php';
-$stmt_cols = $pdo->query("DESCRIBE `related_data`");
-$table_columns = $stmt_cols->fetchAll(PDO::FETCH_COLUMN);
-print_r($table_columns);
-?>
--- htdocs/tmp_check_user_types.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'includes/database.php';
-$st = $pdo->query('SELECT DISTINCT user_type FROM system_users');
-print_r($st->fetchAll(PDO::FETCH_COLUMN));
